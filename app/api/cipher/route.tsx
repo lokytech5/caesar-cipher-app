a65dc3be69ed092da9b85b699b1f18332c251d5f
lokytech5 <lokytech5@gmail.com>
2024-03-19 17:36:35 +0100

    Added chiper logic to route file

--- app/api/cipher/route.tsx
+++ app/api/cipher/route.tsx
@@ -1,9 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json("Hello world")
+
+export async function POST(request: NextRequest) {
+    const { text, shift_amount, direction } = await request.json();
+
+    if (!text) {
+        return NextResponse.json({ error: 'Text is required' }, {status: 404})
+    }
+
+    if (shift_amount === undefined) {
+        return NextResponse.json({ error: "Shift amount is required" }, {status: 404});
+    }
+
+    if (!direction) {
+        return NextResponse.json({ error: "Direction is required" }, { status: 400});
+    }
+
+    const result = caesarCipher(text, shift_amount, direction);
+    return NextResponse.json({ result }, {status: 200});
 }
 
-export async function POST(request: NextRequest){
-    const body = await request.json()
+function caesarCipher(text: string, shift_amount: number, direction: string): string {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    let resultText = "";
+
+    if (direction === "decode"){
+        shift_amount = -shift_amount;
+    }
+
+    for (let char of text){
+        if(alphabet.includes(char)){
+            let currentPosition = alphabet.indexOf(char);
+            let newPosition = (currentPosition + shift_amount + 26) % 26;
+            resultText += alphabet[newPosition];
+        } else {
+            resultText += char;
+        }
+    }
+
+    return resultText;
 }
+
