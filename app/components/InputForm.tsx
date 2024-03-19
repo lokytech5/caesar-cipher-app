d24acfeeed3dd94c81195f65625128e99c507d76
lokytech5 <lokytech5@gmail.com>
2024-03-19 19:01:55 +0100

    added hook reslovers and set up inputForm

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from 'react-query';
+import { cipherSchema } from './cipherSchema';
 
 const InputForm = () => {
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        resolver: zodResolver(cipherSchema)
+    })
+
   return (
     <div>InputForm</div>
   )
