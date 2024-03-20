db6a763fd2b20197f5b636893c1e4d07b6338704
lokytech5 <lokytech5@gmail.com>
2024-03-20 10:06:17 +0100

    code is working as expected

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from 'react-query';
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import InputForm from "./components/InputForm";
+
 export default function Home() {
   return (
-    <main>
-     
-    </main>
+     <div>
+      <h1>Caesar Cipher</h1>
+      <InputForm />
+    </div>
   )
 }
